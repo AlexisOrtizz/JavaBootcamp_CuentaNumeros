05e18a5e07ac5f3e49dd09eee381efc902e7373b
Alexis Ortiz <olmedo.aortiz@gmail.com>
2022-06-20 15:52:58 -0400

    [MOD] Se agregó comentarios al ejercicio para mejor comprensión.

--- src/main/java/com/roshka/bootcamp/SmallerNumberSameDigits.java
+++ src/main/java/com/roshka/bootcamp/SmallerNumberSameDigits.java
@@ -1,8 +1,27 @@
 package com.roshka.bootcamp;
 
+import java.util.Arrays;
+
+import static java.util.Arrays.sort;
+
 public class SmallerNumberSameDigits {
     public static long nextSmaller(long n)
     {
-        return n;
+        String textOrigen = String.valueOf(n);
+        char [] arrOrigen = textOrigen.toCharArray();
+        sort(arrOrigen);
+
+        for(long i = n-1; i >= 1; i--) {
+            String textFin = String.valueOf(i);
+            char [] arrFin = textFin.toCharArray();
+            sort(arrFin);
+
+            if(Arrays.equals(arrOrigen, arrFin)) {
+                return i;
+            }
+        }
+
+        return -1;
     }
+
 }
